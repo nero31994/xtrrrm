739fcad2b9dde02ccff6f5bb96a087569e1a1f04
nero31994 <arenaofvalorph937@gmail.com>
2025-02-07 01:21:21 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,142 +1,29 @@
-const channels = [
-    {
-        name: "TV5",
-        logo: "https://i.imgur.com/jsCBRq0.png",
-        url: "https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/tv5_hd.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "2615129ef2c846a9bbd43a641c7303ef": "07c7f996b1734ea288641a68e1cfdc4d"
-                }
-            }
-        }
-    },
-    {
-        name: "ONE SPORTS",
-        logo: "https://i.imgur.com/btiNwYt.png",
-        url: "https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/cg_onesports_hd.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "53c3bf2eba574f639aa21f2d4409ff11": "3de28411cf08a64ea935b9578f6d0edd"
-                }
-            }
-        }
-    },
-    {
-        name: "RPTV",
-        logo: "https://i.imgur.com/IDCHfXm.png",
-        url: "https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cnn_rptv_prod_hd.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "1917f4caf2364e6d9b1507326a85ead6": "a1340a251a5aa63a9b0ea5d9d7f67595"
-                }
-            }
-        }
-    },
-    {
-        name: "SARI-SARI",
-        logo: "https://i.imgur.com/kKuO7LD.png",
-        url: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_sari_sari_sd.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "0a7ab3612f434335aa6e895016d8cd2d": "b21654621230ae21714a5cab52daeb9d"
-                }
-            }
-        }
-    },
-    {
-        name: "BUKO CH",
-        logo: "https://i.imgur.com/Du6LQCi.png",
-        url: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_buko_sd.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "d273c085f2ab4a248e7bfc375229007d": "7932354c3a84f7fc1b80efa6bcea0615"
-                }
-            }
-        }
-    },
-    {
-        name: "ONE NEWS",
-        logo: "https://i.imgur.com/bmP06bk.png",
-        url: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/onenews_hd1.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "d39eb201ae494a0b98583df4d110e8dd": "6797066880d344422abd3f5eda41f45f"
-                }
-            }
-        }
-    },
-    {
-        name: "ONE PH",
-        logo: "https://i.imgur.com/9dMuFE1.png",
-        url: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/oneph_sd.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "92834ab4a7e1499b90886c5d49220e46": "a7108d9a6cfcc1b7939eb111daf09ab3"
-                }
-            }
-        }
-    },
-    {
-        name: "TRUE FM TV",
-        logo: "https://i.imgur.com/U8L0Liq.png",
-        url: "https://qp-pldt-live-grp-08-prod.akamaized.net/out/u/truefm_tv.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "0559c95496d44fadb94105b9176c3579": "40d8bb2a46ffd03540e0c6210ece57ce"
-                }
-            }
-        }
-    },
-    {
-        name: "MEDIA PILIPINAS TV",
-        logo: "https://i.imgur.com/BMCnoRn.png",
-        url: "https://qp-pldt-live-grp-09-prod.akamaized.net/out/u/cg_mptv.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "6aab8f40536f4ea98e7c97b8f3aa7d4e": "139aa5a55ade471faaddacc4f4de8807"
-                }
-            }
-        }
-    },
-    {
-        name: "PEOPLE'S TELEVISION",
-        logo: "https://i.imgur.com/ycPz1Uc.png",
-        url: "https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/cg_ptv4_sd.mpd",
-        drm: {
-            widevine: { url: "https://your-widevine-license-server.com" },
-            playready: { url: "https://your-playready-license-server.com" },
-            clearkey: {
-                keys: {
-                    "71a130a851b9484bb47141c8966fb4a3": "ad1f003b4f0b31b75ea4593844435600"
-                }
-            }
-        }
-    }
-];
+const channels = {
+
+GTV: {
+        name: 'GTV',
+        url: 'https://ott.studentsdemo.com/stream/phcathenatv/gtv/master.m3u8?u=phc&p=1ed3395bba4d481a74e631943df9643663da8f300a8edececcb8b7da46349f01',
+        type: 'hls'
+         }, 
+GMA: {
+        name: 'GMA',
+        url: 'https://ott.studentsdemo.com/stream/phcathenatv/gma/master.m3u8?u=phc&p=1ed3395bba4d481a74e631943df9643663da8f300a8edececcb8b7da46349f01',
+        type: 'hls'
+         }, 
+KAP: {
+        name: 'KAPAMILYA',
+        url: 'https://ott.studentsdemo.com/stream/phcathenatv/kapamilyachannel/master.m3u8?u=phc&p=1ed3395bba4d481a74e631943df9643663da8f300a8edececcb8b7da46349f01',
+        type: 'hls'
+         }, 
+KAPAMILYA_CHANNEL: {
+    name: 'JUST FOR LAUGH',
+    url: 'https://epg.provider.plex.tv/library/parts/5e20b730f2f8d5003d739db7-64704954e536e0faaf10be9f/variant.m3u8?x-plex-token=sE38z2Coa9z6BhtRqbPm&x-plex-advertising-identifier=&x-plex-advertising-donottrack=0&x-plex-drm=&x-plex-device-screen-resolution=&x-plex-device-vendor=&x-plex-version=&x-plex-session-id=&x-plex-playback-session-id=&x-plex-playback-id=p_9b287a21-cb74-4905-9920-cf78e8c152ce&x-plex-provider-streaming-start=1737857606&url=6883812ad10c39c69cb45a329fc009f5-7bd6f54c7d9982a1d715addcce6a30e2eb4ab46e05e3bde6374112dd6f24fe170b7432a2f76f373805d3c3fe658817e8ef7ac95894134989582236d7f344d2e1296f3ba13377bb38c88aeceaea2072d8846a61c0efc9a3068f77b1d67550ee00ff81bf74e5112be0790d7d6557a7afcf1a4aa8d5876739e0babcb3c56b90916f2d60860c043b5c0f4d5d9805471b3d46fbe7e2477164ad9fa9a77134fa532dd3cf5456e1099c7581008d555bb6e57e9d57707aac75a6c4e056145b99145d5627e44695ca1f66f5919c19edb53d7273a5',
+    type: 'hls',
+    
+},
+jungo: {
+        name: 'JUNGO PINOY TV',
+        url: 'https://jungotvstream-chanall.akamaized.net/jungotv/jungopinoytv/playlist_1080p.m3u8',
+        type: 'hls'
+         }, 
+};
