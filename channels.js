a7df9a6bfeb09d9b6616e38d9a7f90ae038b9bc4
nero31994 <arenaofvalorph937@gmail.com>
2025-02-07 10:28:51 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,90 +1,82 @@
-const channels = [
-    {
+window.channels = {
+    "tv5": {
         name: "TV5",
-        streamUrl: "https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/tv5_hd.mpd",
-        image: "https://i.imgur.com/jsCBRq0.png",
+        url: "https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/tv5_hd.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "2615129ef2c846a9bbd43a641c7303ef": "07c7f996b1734ea288641a68e1cfdc4d"
-            }
+            keyId: "2615129ef2c846a9bbd43a641c7303ef", // ClearKey Key ID
+            key: "07c7f996b1734ea288641a68e1cfdc4d"  // ClearKey Key
         }
     },
-    {
+    "one_sports": {
         name: "ONE SPORTS",
-        streamUrl: "https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/cg_onesports_hd.mpd",
-        image: "https://i.imgur.com/btiNwYt.png",
+        url: "https://qp-pldt-live-grp-07-prod.akamaized.net/out/u/cg_onesports_hd.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "53c3bf2eba574f639aa21f2d4409ff11": "3de28411cf08a64ea935b9578f6d0edd"
-            }
+            keyId: "53c3bf2eba574f639aa21f2d4409ff11", // ClearKey Key ID
+            key: "3de28411cf08a64ea935b9578f6d0edd"  // ClearKey Key
         }
     },
-    {
+    "rptv": {
         name: "RPTV",
-        streamUrl: "https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cnn_rptv_prod_hd.mpd",
-        image: "https://i.imgur.com/IDCHfXm.png",
+        url: "https://qp-pldt-live-grp-03-prod.akamaized.net/out/u/cnn_rptv_prod_hd.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "1917f4caf2364e6d9b1507326a85ead6": "a1340a251a5aa63a9b0ea5d9d7f67595"
-            }
+            keyId: "1917f4caf2364e6d9b1507326a85ead6", // ClearKey Key ID
+            key: "a1340a251a5aa63a9b0ea5d9d7f67595"  // ClearKey Key
         }
     },
-    {
+    "sari_sari": {
         name: "SARI-SARI",
-        streamUrl: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_sari_sari_sd.mpd",
-        image: "https://i.imgur.com/kKuO7LD.png",
+        url: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_sari_sari_sd.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "0a7ab3612f434335aa6e895016d8cd2d": "b21654621230ae21714a5cab52daeb9d"
-            }
+            keyId: "0a7ab3612f434335aa6e895016d8cd2d", // ClearKey Key ID
+            key: "b21654621230ae21714a5cab52daeb9d"  // ClearKey Key
         }
     },
-    {
+    "buko_ch": {
         name: "BUKO CH",
-        streamUrl: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_buko_sd.mpd",
-        image: "https://i.imgur.com/Du6LQCi.png",
+        url: "https://qp-pldt-live-grp-06-prod.akamaized.net/out/u/cg_buko_sd.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "d273c085f2ab4a248e7bfc375229007d": "7932354c3a84f7fc1b80efa6bcea0615"
-            }
+            keyId: "d273c085f2ab4a248e7bfc375229007d", // ClearKey Key ID
+            key: "7932354c3a84f7fc1b80efa6bcea0615"  // ClearKey Key
         }
     },
-    {
+    "one_news": {
         name: "ONE NEWS",
-        streamUrl: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/onenews_hd1.mpd",
-        image: "https://i.imgur.com/bmP06bk.png",
+        url: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/onenews_hd1.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "d39eb201ae494a0b98583df4d110e8dd": "6797066880d344422abd3f5eda41f45f"
-            }
+            keyId: "d39eb201ae494a0b98583df4d110e8dd", // ClearKey Key ID
+            key: "6797066880d344422abd3f5eda41f45f"  // ClearKey Key
         }
     },
-    {
+    "one_ph": {
         name: "ONE PH",
-        streamUrl: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/oneph_sd.mpd",
-        image: "https://i.imgur.com/9dMuFE1.png",
+        url: "https://qp-pldt-live-grp-04-prod.akamaized.net/out/u/oneph_sd.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "92834ab4a7e1499b90886c5d49220e46": "a7108d9a6cfcc1b7939eb111daf09ab3"
-            }
+            keyId: "92834ab4a7e1499b90886c5d49220e46", // ClearKey Key ID
+            key: "a7108d9a6cfcc1b7939eb111daf09ab3"  // ClearKey Key
         }
     },
-    {
+    "true_fm_tv": {
         name: "TRUE FM TV",
-        streamUrl: "https://qp-pldt-live-grp-08-prod.akamaized.net/out/u/truefm_tv.mpd",
-        image: "https://i.imgur.com/U8L0Liq.png",
+        url: "https://qp-pldt-live-grp-08-prod.akamaized.net/out/u/truefm_tv.mpd",
+        type: "mpd",
         drm: {
             type: "clearkey",
-            keys: {
-                "0559c95496d44fadb94105b9176c3579": "40d8bb2a46ffd03540e0c6210ece57ce"
-            }
+            keyId: "0559c95496d44fadb94105b9176c3579", // ClearKey Key ID
+            key: "40d8bb2a46ffd03540e0c6210ece57ce"  // ClearKey Key
         }
     }
-    ];
+};
